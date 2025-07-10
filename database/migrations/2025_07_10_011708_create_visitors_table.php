4de4792a9c88e3a280a13a71ca54109ba460f003
Fresnel Siregar <fresnel@example.com>
2025-07-10 15:51:57 +0700

    feat: db migration

---
+++ database/migrations/2025_07_10_011708_create_visitors_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('visitors', function (Blueprint $table) {
+            $table->id();
+            $table->string('nik');
+            $table->text('id_card_photo')->nullable();
+            $table->string('full_name', 255);
+            $table->string('company', 255)->nullable();
+            $table->string('phone', 255)->nullable();
+            $table->string('department_purpose', 255)->nullable();
+            $table->string('section_purpose', 255)->nullable();
+            $table->string('self_photo', 255)->nullable();
+            $table->dateTime('visit_datetime');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('visitors');
+    }
+};
